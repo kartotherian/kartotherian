fa464037de9b2db9c81fd8bb71de1d51fa173971
Mike Morris <michael.patrick.morris@gmail.com>
2013-12-12 15:39:23 -0500

    add broken tm2z fixtures and tests

--- test/tm2z.js
+++ test/tm2z.js
@@ -9,36 +9,15 @@ var path = require('path');
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 var crypto = require('crypto');
-// var imageEqualsFile = require('./image.js');
 
 function md5(str) {
     return crypto.createHash('md5').update(str).digest('hex');
 }
 
-function gzip(path, callback) {
-    var gzPath = path + '.gz';
-    fs.createReadStream(path)
-        .pipe(zlib.createGzip())
-        .pipe(fs.createWriteStream(gzPath))
-        .on('close', function() {
-            callback(null, gzPath);
-        })
-        .on('error', callback);
-}
-
 // Load fixture data.
 var fixtureDir = path.resolve(__dirname + '/fixtures/tm2z'),
     remotePath = 'http://mapbox.s3.amazonaws.com/tilelive-vector/test-tm2z.tm2z',
-    xml = fs.readFileSync(fixtureDir + '/project.xml');
-
-/*
-// Additional error tile fixtures.
-zlib.deflate(new Buffer('asdf'), function(err, deflated) {
-    if (err) throw err;
-    tiles.a['1.0.2'] = new Buffer('asdf'); // invalid deflate
-    tiles.a['1.0.3'] = deflated;           // invalid protobuf
-});
-*/
+    xml = fs.readFileSync(fixtureDir + '/project/project.xml');
 
 // Register vector:, tm2z:, tm2z+http: and mapbox: tilelive protocols
 require('..').registerProtocols(tilelive);
@@ -56,58 +35,43 @@ describe('tm2z', function() {
     it('matches expected xml', function(done) {
         tilelive.load('tm2z://' + fixtureDir + '/project.tm2z', function(err, source) {
             if (err) throw err;
-            assert.equal(xml, source._xml);
+            assert.equal(source._xml, xml);
             done();
         });
     });
-    it('unpacks without gzip', function(done) {
+    it('gunzips then untars', function(done) {
+        tilelive.load('tm2z://' + fixtureDir + '/project.tar.gz', function(err, source) {
+            if (err) throw err;
+            done();
+        });
+    });
+    it('errors out if not gzipped', function(done) {
         tilelive.load('tm2z://' + fixtureDir + '/project.tar', function(err, source) {
-            if (err) throw err;
+            assert.equal(err.code, 'Z_DATA_ERROR');
+            assert.equal(err.message, 'incorrect header check');
             done();
         });
     });
-    /* -------------------------------------------------- */
-    /* -------------------------------------------------- */
-    it('errors out if not packed with tar', function(done) {
-        tilelive.load('tm2z://' + fixtureDir + '/project.gz', function(err, source) {
-            assert.equal('invalid tar file', err.message);
+    it('errors out on bad gunzip', function(done) {
+        tilelive.load('tm2z://' + fixtureDir + '/doublezip.tar.gz', function(err, source) {
+            assert.equal(err.message, 'invalid tar file');
             done();
         });
     });
-    it('errors out if gzipped before tar', function(done) {
-        tilelive.load('tm2z://' + fixtureDir + '/project.gz.tar', function(err, source) {
-            assert.equal('EISDIR', err.code);
+    it('errors out if missing project.xml', function(done) {
+        tilelive.load('tm2z://' + fixtureDir + '/empty.tar.gz', function(err, source) {
+            assert.equal(err.message, 'project.xml not found in package');
             done();
         });
     });
-    it('gzips, gunzips then untars .tar fixture', function(done) {
-        gzip(fixtureDir + '/project.tar', function(err, path) {
-            if (err) throw err;
-            tilelive.load('tm2z://' + path, function(err, source) {
-                console.log(err, source);
-                if (err) throw err;
-                done();
-            });
-        });
-    });
-    /*
-    it('errors out on bad deflate', function(done) {
-        zlib.deflate(new Buffer('asdf'), function(err, deflated) {
-            if (err) throw err;
-            sources.a.getTile(1, 0, 2, function(err) {
-                assert.equal('Z_DATA_ERROR', err.code);
-                done();
-            });
-        });
-    });
-    it('errors out on bad protobuf', function(done) {
-        sources.a.getTile(1, 0, 3, function(err) {
-            assert.equal('could not parse protobuf', err.message);
+    it('errors out on invalid project.xml', function(done) {
+        tilelive.load('tm2z://' + fixtureDir + '/malformed.tar.gz', function(err, source) {
+            assert.equal(err.message.split(':')[0], 'XML document not well formed');
             done();
         });
     });
-    */
 });
+
 describe('tm2z+http', function() {
     it('loads a tm2z+http url', function(done) {
         this.timeout(5000);
