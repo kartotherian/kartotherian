1be904404cdcb60fa45014e380cbcc551c4e403d
Young Hahn <young@developmentseed.org>
2014-05-08 17:27:59 -0400

    Bad z.

--- index.js
+++ index.js
@@ -245,7 +245,7 @@ Vector.prototype.getInfo = function(callback) {
 // letting the tilelive-vector backend do the hard work of finding
 // the right tile to use.
 Vector.prototype.queryTile = function(z, lon, lat, options, callback) {
-    var xyz = sm.xyz([lon, lat, lon, lat], 22);
+    var xyz = sm.xyz([lon, lat, lon, lat], z);
     this._backend.getTile(z, xyz.minX, xyz.minY, function(err, vtile, headers) {
         if (err) return callback(err);
         try {
