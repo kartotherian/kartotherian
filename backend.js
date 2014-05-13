72ead637b7eecb718633bb2d07895eb6de413d02
Camille Teicheira <cteicheira@gmail.com>
2014-05-13 12:27:16 -0400

    remove backend scale adjustment #53

--- backend.js
+++ backend.js
@@ -56,12 +56,9 @@ Backend.prototype.getTile = function(z, x, y, callback) {
     var scale = callback.scale || backend._scale;
     var now = +new Date;
 
-    // If scale > 1 adjusts source data zoom level inversely.
-    // scale 2x => z-1, scale 4x => z-2, scale 8x => z-3, etc.
-    var d = Math.round(Math.log(scale)/Math.log(2));
-    var bz = (z - d) > backend._minzoom ? z - d : backend._minzoom;
-    var bx = Math.floor(x / Math.pow(2, z - bz));
-    var by = Math.floor(y / Math.pow(2, z - bz));
+    var bz = z;
+    var bx = x;
+    var by = y;
 
     // Overzooming support.
     if (bz > backend._maxzoom) {
