424880c36de2ccf7f6719d308e65f77026bdd715
Camille Teicheira <cteicheira@gmail.com>
2014-06-02 13:24:16 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,187 @@
+var sm = new (require('sphericalmercator'));
+var queue = require('queue-async');
+var blend = require('blend');
+
+module.exports = abaculus;
+
+var limit = 19008;
+
+function abaculus(arg, callback){
+    var z = arg.zoom || 1,
+        s = arg.scale || 1,
+        center = arg.center || null,
+        corners = arg.corners || null,
+        getTile = arg.getTile || null,
+        format = arg.format || 'png';
+    if (!getTile) 
+        return callback(new Error('Invalid function for getting tiles'));
+
+    if (center) {
+        // get center coordinates in px from lng,lat
+        abaculus.coordsFromCenter(z, s, center, function(err, center){
+            if (err) return callback(err);
+            // generate list of tile coordinates center
+            abaculus.tileList(z, s, center, function(err, coords){
+                if (err) return callback(err);
+                // get tiles based on coordinate list and stitch them together
+                abaculus.stitchTiles(coords, format, getTile, callback);
+            });
+        });
+    } else if (corners) {
+        // get center coordinates in px from ne & sw corners
+        abaculus.coordsFromCorners(z, s, corners, function(err, center){
+            if (err) return callback(err);
+            // generate list of tile coordinates center
+            abaculus.tileList(z, s, center, function(err, coords){
+                if (err) return callback(err);
+                // get tiles based on coordinate list and stitch them together
+                abaculus.stitchTiles(coords, format, getTile, callback);
+            });
+        });
+    } else {
+        return callback(new Error('No coordinates provided.'));
+    }
+};
+
+abaculus.coordsFromCorners = function(z, s, corners, done){
+    sm.size = 256 * s;
+    topLeft = sm.px([corners.topLeft.x, corners.topLeft.y], z);
+    bottomRight = sm.px([corners.bottomRight.x, corners.bottomRight.y], z);
+    var center = {};
+    center.w = bottomRight[0] - topLeft[0];
+    center.h = bottomRight[1] - topLeft[1];
+    if (center.w <= 0 || center.h <= 0) return done(new Error('Incorrect coordinates -- bottom right corner must be lower and to the east of the top left corner'));
+    
+    var origin = [topLeft[0] + center.w/2, topLeft[1] + center.h/2];
+    center.x = origin[0];
+    center.y = origin[1];
+    center.w = center.w * s;
+    center.h = center.h * s;
+
+    if (center.w >= limit || center.h >= limit) return done(new Error('Desired image is too large.'));
+    return done(null, center);
+}
+
+abaculus.coordsFromCenter = function(z, s, center, done){
+    var origin = sm.px([center.x, center.y], z);
+    center.x = origin[0];
+    center.y = origin[1];
+    center.w = center.w * s;
+    center.h = center.h * s;
+
+    if (center.w >= limit || center.h >= limit) return done(new Error('Desired image is too large.'));
+    return done(null, center);
+}
+
+// Generate the zxy and px/py offsets needed for each tile in a static image.
+// x, y are center coordinates in pixels
+abaculus.tileList = function(z, s, center, callback) {
+    var x = center.x,
+        y = center.y,
+        w = center.w,
+        h = center.h;
+    var dimensions = {x: w, y: h};
+    var tileSize = 256 * s;
+
+    var centerCoordinate = {
+            column: x / 256,
+            row: y / 256,
+            zoom: z
+        };
+
+    function pointCoordinate(point) {
+        var coord = { column: centerCoordinate.column,
+                    row: centerCoordinate.row,
+                    zoom: centerCoordinate.zoom,
+                    };
+        coord.column += (point.x - w / 2) / tileSize;
+        coord.row += (point.y - h / 2) / tileSize;
+        return coord;
+    }
+
+    function coordinatePoint(coord) {
+        // Return an x, y point on the map image for a given coordinate.
+        if (coord.zoom != z) coord = coord.zoomTo(zoom);
+        return {
+            x: w / 2 + tileSize * (coord.column - centerCoordinate.column),
+            y: h / 2 + tileSize * (coord.row - centerCoordinate.row)
+        };
+    }
+
+    function floorObj(obj) {
+        return {
+                row: Math.floor(obj.row),
+                column: Math.floor(obj.column),
+                zoom: Math.floor(obj.zoom)
+            };
+    }
+
+    var tl = floorObj(pointCoordinate({x: 0, y:0}));
+    var br = floorObj(pointCoordinate(dimensions));
+    coords = {};
+    coords.tiles = [];
+    var tileCount = (br.column - tl.column + 1) * (br.row - tl.row + 1);
+
+    for (var column = tl.column; column <= br.column; column++) { 
+        for (var row = tl.row; row <= br.row; row++) {
+            var c = { column: column,
+                    row: row,
+                    zoom: z,
+                    };
+            var p = coordinatePoint(c);
+
+            // Wrap tiles with negative coordinates.
+            c.column = c.column < 0 ?
+                Math.pow(2,c.zoom) + c.column :
+                c.column % Math.pow(2,c.zoom);
+
+            if (c.row < 0) continue;
+            coords.tiles.push({
+                z: c.zoom,
+                x: c.column,
+                y: c.row,
+                px: Math.round(p.x),
+                py: Math.round(p.y)
+            });
+        }
+    }
+    coords.dimensions = { x: w, y: h };
+    coords.center = floorObj(centerCoordinate);
+    coords.scale = s;
+
+    return callback(null, coords);
+}
+
+abaculus.stitchTiles = function(coords, format, getTile, callback){
+    if (!coords) return callback(new Error('No coords object.'));
+    var tileQueue = new queue(1);
+    var dat = [];
+    var w = coords.dimensions.x,
+        h = coords.dimensions.y,
+        s = coords.scale,
+        tiles = coords.tiles;
+
+    tiles.forEach(function(t){
+        tileQueue.defer(function(z, x, y, done){
+            done.scale = s;
+            done.format = format;
+            // getTile is a function that returns 
+            // a tile given z, x, y, & done
+            getTile(z, x, y, done);
+        }, t.z, t.x, t.y);
+    });
+
+    function tileQueueFinish(err, data) {
+        if (err) console.log(err, data);
+        if (!data) return callback(new Error('No tiles to stitch.'));
+        data.forEach(function(d, i){
+            dat.push({buffer: d, x: tiles[i].px, y: tiles[i].py});
+        });
+        blend(dat, {
+            width: w,
+            height: h
+        }, callback);
+    }
+
+    tileQueue.awaitAll(tileQueueFinish);
+}
---
+++ test/test.js
@@ -0,0 +1,186 @@
+var assert = require('assert');
+var printer = require('../index.js');
+var fs = require('fs');
+var path = require('path');
+
+// defaults
+var zoom = 5,
+	scale = 4,
+	x = 4096,
+	y = 4096;
+
+describe('Get center from corner coords', function(){
+	it('should fail if (x2,y2) is to the north or west of (x1, y1)', function(done){
+		var corners = {
+			topLeft: {
+				x: 0,
+				y: 0
+			},
+			bottomRight: {
+				x: -1,
+				y: 1
+			}
+		};
+		printer.coordsFromCorners(zoom, scale, corners, function(err){
+			assert.equal(err.message, 'Incorrect coordinates -- bottom right corner must be lower and to the east of the top left corner');
+			done();
+		});
+	});
+	it('should fail if (x1, y1) and (x2,y2) are equal', function(done){
+		var corners = {
+			topLeft: {
+				x: 0,
+				y: 0
+			},
+			bottomRight: {
+				x: 0,
+				y: 0
+			}
+		};
+		printer.coordsFromCorners(zoom, scale, corners, function(err){
+			assert.equal(err.message, 'Incorrect coordinates -- bottom right corner must be lower and to the east of the top left corner');
+			done();
+		});
+	});
+	it('should fail if the image is too large', function(done){
+		var corners = {
+			topLeft: {
+				x: -60,
+				y: 60
+			},
+			bottomRight: {
+				x: 60,
+				y: -60
+			}
+		};
+		var center = printer.coordsFromCorners(7, 2, corners, function(err){
+			assert.equal(err.message, 'Desired image is too large.');
+			done();
+		});
+	});
+	it('should return the correct coordinates', function(done){
+		var corners = {
+			topLeft: {
+				x: -60,
+				y: 60
+			},
+			bottomRight: {
+				x: 60,
+				y: -60
+			}
+		};
+		printer.coordsFromCorners(zoom, scale, corners, function(err, center){
+			assert.deepEqual(center.w, 10920);
+			assert.deepEqual(center.h, 13736);
+			assert.deepEqual(center.x, x);
+			assert.deepEqual(center.y, y);
+			done();
+		});
+	});
+});
+
+describe('get coordinates from center', function(){
+	it('should should fail if the image is too large', function(done){
+		var center = {
+			x: 0,
+			y: 0,
+			w: 4752,
+			h: 4752
+		};
+		printer.coordsFromCenter(zoom, scale, center, function(err){
+			assert.equal(err.message, 'Desired image is too large.');
+			done();
+		});
+	});
+	it('should return correct origin coords', function(done){
+		var center = {
+			x: 0,
+			y: 0,
+			w: 800,
+			h: 800
+		};
+		printer.coordsFromCenter(zoom, scale, center, function(err, center){
+			assert.equal(center.x, x);
+			assert.equal(center.y, y);
+			done();
+		});
+	});
+});
+
+describe('create list of tile coordinates', function(){
+	var expectedTiles = { 
+		tiles: [{ z: 5, x: 15, y: 15, px: -112, py: -108 },
+			    { z: 5, x: 15, y: 16, px: -112, py: 916 },
+			    { z: 5, x: 16, y: 15, px: 912, py: -108 },
+			    { z: 5, x: 16, y: 16, px: 912, py: 916 } ],
+  		dimensions: { x: 1824, y: 1832 },
+ 		center: { row: 16, column: 16, zoom: 5 },
+ 		scale: 4
+ 	};
+ 	var center =  {x: x, y: y, w: 1824, h: 1832 }
+	it('should return a tiles object with correct coords', function(done){
+		printer.tileList(zoom, scale, center, function(err, tiles){
+			assert.deepEqual(JSON.stringify(tiles), JSON.stringify(expectedTiles));
+			done();
+		});
+	});
+});
+
+describe('stitch tiles into single png', function(){
+	var expectedTiles = { 
+		tiles: [ { z: 1, x: 0, y: 0, px: 0, py: 0 },
+			     { z: 1, x: 0, y: 1, px: 0, py: 256 },
+			     { z: 1, x: 1, y: 0, px: 256, py: 0 },
+			     { z: 1, x: 1, y: 1, px: 256, py: 256 }],
+			dimensions: { x: 512, y: 512 },
+		  	center: { row: 1, column: 1, zoom: 1 },
+		  	scale: 2
+		};
+
+	it('should fail if no coordinates object', function(done){
+		printer.stitchTiles(null, 'png', function(){}, function(err){
+			assert.equal(err.message, 'No coords object.');
+			done();
+		});
+	});
+	it('should fail if no valid tileGetter', function(done){
+		printer.stitchTiles(expectedTiles, 'png', getTileFake, function(err){
+			assert.equal(err.message, 'No tiles to stitch.');
+			done();
+		});
+	});
+	// need different fixtures for this
+	// it('should return tiles and stitch them together', function(done){
+	// 	st.stitchTiles(expectedTiles, 'png', getTileTest, function(err, image){
+	// 		assert();
+	// 		done();
+	// 	});
+	// });
+});
+
+var tiles = fs.readdirSync(path.resolve(__dirname + '/fixtures')).reduce(function(memo, basename) {
+		        var key = basename.split('.').slice(0,3).join('.');
+		        memo[key] = fs.readFileSync(path.resolve(__dirname + '/fixtures/' + basename));
+		        return memo;
+		    }, {});
+
+function getTileTest(z,x,y,callback) {
+    var key = [z,x,y].join('.');
+
+    // Headers.
+    var headers = {
+        'Last-Modified': new Date().toUTCString(),
+        'ETag':'73f12a518adef759138c142865287a18',
+        'Content-Type':'application/x-protobuf'
+    };
+
+    if (!tiles[key]) {
+        return callback(new Error('Tile does not exist'));
+    } else {
+        return callback(null, tiles[key], headers);
+    }
+};
+
+function getTileFake(z, x, y, callback){
+	return callback('wat');
+}
