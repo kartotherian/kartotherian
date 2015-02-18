22388132ec244fc55f038756454789c2bce8b402
Marko Obrovac <marko.obrovac@gmail.com>
2015-02-18 17:53:19 +0100

    Update the /v1 example
    The /v1/conf example is gone. Instead, there is /v1/wiki(/uri) which
    shows various techniques likely to be needed by service developers (see
    the full description in the header of the example route)

--- routes/v1.js
+++ routes/v1.js
@@ -2,8 +2,7 @@
 
 
 var express = require('express');
-var fs = BBPromise.promisifyAll(require('fs'));
-var yaml = require('js-yaml');
+var preq = require('preq');
 
 
 /**
@@ -18,18 +17,72 @@ var app;
 
 
 /**
- * GET /conf
- * Reads and returns the configuration file used to
- * start the service. IMPORTANT NOTE: This is a sample
- * only for demostrating promises, you should not
- * expose this endpoint in real services.
+ * GET/POST/HEAD /wiki{/url}
+ * Fetches site info for a wiki with the given URI. This example shows how to:
+ * 1) manage multiple methods for the same URI (using router.all)
+ * 2) use named URI parameters (by prefixing them with a double colon)
+ * 3) use optional URI parameters (by suffixing them with a question mark)
+ * 4) extract URI and query parameters, as well as body data
+ * 5) issue external requests
+ * 6) use Promises to achieve (5) and return the result
+ *
+ * There are multiple ways of calling this endpoint:
+ * 1) GET /v1/wiki/en.wikipedia.org
+ * 2) GET /v1/wiki?uri=en.wikipedia.org
+ * 3) POST /v1/wiki (with body: uri=en.wikipedia.org)
+ * 4) HEAD (with the URI as in (1) or (2))
  */
-router.get('/conf', function(req, res) {
+router.all('/wiki/:uri?', function(req, res) {
 
-    fs.readFileAsync(__dirname + '/../config.yaml')
-    .then(function(src) {
-        var config = yaml.safeLoad(src);
-        res.json(config);
+    var uri;
+    var method = req.method.toLowerCase();
+
+    // first, allow only GET, POST and HEAD methods, and
+    // response with a 200 for HEAD right away
+    if(method === 'head') {
+        res.status(200).set('Connection', 'Close').end();
+        return; // needs to be here!
+    } else if(!/^(get|post)$/.test(method)) {
+        res.status(404).end();
+        return; // needs to be here!
+    }
+
+    // let's try to find out how did the user supply the parameter
+    if(req.params && req.params.uri) {
+        // the caller used the optional path segment
+        uri = req.params.uri;
+    } else if(req.query && req.query.uri) {
+        // the user supplied the URI in the query parameter
+        uri = req.query.uri;
+    } else if(req.body && req.body.uri) {
+        // the users issued a POST request and put the URI in the body
+        uri = req.body.uri;
+    } else {
+        // nothing of the above, error out
+        res.status(400).end();
+        return; // don't forget this one!
+    }
+
+    // construct the request
+    var apiReq = {
+        uri: 'http://' + uri + '/w/api.php' ,
+        body: {
+            format: 'json',
+            action: 'query',
+            meta: 'siteinfo',
+            continue: ''
+        }
+    };
+
+    // send it
+    // NOTE: preq uses bluebird, so we can safely chain it with a .then() call
+    preq.post(apiReq)
+    // and then return the result to the caller
+    .then(function(apiRes) {
+        // preq returns the parsed object, so we can use res.json
+        // to send the response body back to the client
+        // set the response code as returned by the MW API
+        res.status(apiRes.status).json(apiRes.body);
     });
 
 });
