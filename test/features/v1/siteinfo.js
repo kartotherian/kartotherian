c0e155179c088ea6403cc7faabc32ff20ce1006e
Marko Obrovac <marko.obrovac@gmail.com>
2015-02-19 17:01:33 +0100

    Add testing capabilities to the template
    This commit adds a simple test suite to the service template.
    Concretely:
    * add dependencies to mocha, jshint and friends
    * add the test and coverage scripts
    * add the helpers (assert.js, server.js, logStream.js)
    * add tests for all of the current routes

---
+++ test/features/v1/siteinfo.js
@@ -0,0 +1,60 @@
+'use strict';
+
+
+// mocha defines to avoid JSHint breakage
+/* global describe, it, before, beforeEach, after, afterEach */
+
+
+var preq   = require('preq');
+var assert = require('../../utils/assert.js');
+var server = require('../../utils/server.js');
+
+
+describe('wiki site info', function() {
+
+    this.timeout(20000);
+
+    before(function () { return server.start(); });
+
+    // common URI prefix for v1
+    var uri = server.config.uri + 'v1/siteinfo/en.wikipedia.org/';
+
+    it('should get all general enwiki site info', function() {
+        return preq.get({
+            uri: uri
+        }).then(function(res) {
+            // check the status
+            assert.status(res, 200);
+            // check the returned Content-Type header
+            assert.contentType(res, 'application/json');
+            // inspect the body
+            assert.notDeepEqual(res.body, undefined, 'No body returned!');
+            assert.notDeepEqual(res.body.server, undefined, 'No server field returned!');
+        });
+    });
+
+    it('should get the mainpage setting of enwiki', function() {
+        return preq.get({
+            uri: uri + 'mainpage'
+        }).then(function(res) {
+            // check the status
+            assert.status(res, 200);
+            // check the returned Content-Type header
+            assert.contentType(res, 'application/json');
+            // inspect the body
+            assert.notDeepEqual(res.body, undefined, 'No body returned!');
+            assert.deepEqual(res.body.mainpage, 'Main Page', 'enwiki mainpage mismatch!');
+        });
+    });
+
+    it('should fail to get a non-existent setting of enwiki', function() {
+        return preq.get({
+            uri: uri + 'dummy_wiki_setting'
+        }).catch(function(res) {
+            // check the status
+            assert.status(res, 404);
+        });
+    });
+
+});
+
---
+++ test/utils/logStream.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var bunyan = require('bunyan');
+
+function logStream() {
+
+  var log = [];
+  var parrot = bunyan.createLogger({
+    name: 'test-logger',
+    level: 'warn'
+  });
+
+  function write(chunk, encoding, callback) {
+    try {
+        var entry = JSON.parse(chunk);
+        var levelMatch = /^(\w+)/.exec(entry.levelPath);
+        if (levelMatch) {
+            var level = levelMatch[1];
+            if (parrot[level]) {
+                parrot[level](entry);
+            }
+        }
+    } catch (e) {
+        console.error('something went wrong trying to parrot a log entry', e, chunk);
+    }
+
+    log.push(chunk);
+  }
+
+  // to implement the stream writer interface
+  function end(chunk, encoding, callback) {
+  }
+
+  function get() {
+    return log;
+  }
+
+  function slice() {
+
+    var begin = log.length;
+    var end   = null;
+
+    function halt() {
+      if (end === null) {
+        end = log.length;
+      }
+    }
+
+    function get() {
+      return log.slice(begin, end);
+    }
+
+    return {
+      halt: halt,
+      get: get
+    };
+
+  }
+
+  return {
+    write: write,
+    end: end,
+    slice: slice,
+    get: get
+  };
+}
+
+module.exports = logStream;
