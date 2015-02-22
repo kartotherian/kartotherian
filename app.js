66006179c77c36ba08ee43c2e52d13434abbe9ad
Yuri Astrakhan <yurik@wikimedia.org>
2015-02-21 22:09:29 -0500

    Reworked app.js
    * Restructured to initApp(), loadRoutes(), createServer()
    * Performs app initialization before route loading
    * Allows all steps to use fully loaded options
    * Default version should probably be 0.1.0 - 0.0.0 looks weird

--- app.js
+++ app.js
@@ -6,95 +6,111 @@ var express = require('express');
 var compression = require('compression');
 var bodyParser = require('body-parser');
 var multer = require('multer');
-var pkg_info = require('./package.json');
+var packageInfo = require('./package.json');
 var fs = BBPromise.promisifyAll(require('fs'));
 
 
 /**
- * The main application object
+ * Promise create an express app and initialize it
+ * @param options
+ * @returns {bluebird}
  */
-var app = express();
+function initApp(options) {
+    return new BBPromise(function (resolve) {
+        // The main application object
+        var app = express();
+        // disable the X-Powered-By header
+        app.set('x-powered-by', false);
+        // disable the ETag header - users should provide them!
+        app.set('etag', false);
+        // enable compression
+        app.use(compression({level: 3}));
+        // use the JSON body parser
+        app.use(bodyParser.json());
+        // use the application/x-www-form-urlencoded parser
+        app.use(bodyParser.urlencoded({extended: true}));
+        // use the multipart/form-data
+        app.use(multer());
+        // serve static files from static/
+        app.use('/static', express.static(__dirname + '/static'));
 
-/* Basic configuration */
-
-// disable the X-Powered-By header
-app.set('x-powered-by', false);
-// disable the ETag header - users should provide them!
-app.set('etag', false);
-// enable compression
-app.use(compression({level: 3}));
-// use the JSON body parser
-app.use(bodyParser.json());
-// use the application/x-www-form-urlencoded parser
-app.use(bodyParser.urlencoded({ extended: true }));
-// use the multipart/form-data
-app.use(multer());
-// serve static files from static/
-app.use('/static', express.static(__dirname + '/static'));
-
-/*** More configuration of app comes here ***/
-
-
-function loadRoutes () {
-
-    // get the list of files in routes/
-    return fs.readdirAsync(__dirname + '/routes')
-    .map(function(fname) {
-        // ... and then load each route
-        // but only if it's a js file
-        if(!/\.js$/.test(fname)) {
-            return;
-        }
-        // import the route file
-        var route = require(__dirname + '/routes/' + fname);
-        route = route(app);
-        // check that the route exports the object we need
-        if(route.constructor !== Object || !route.path || !route.router) {
-            throw new Error('routes/' + fname + ' does not export the correct object!');
-        }
-        // all good, use that route
-        app.use(route.path, route.router);
-    }).then(function() {
-        // route loading is now complete, return the app object
-        return app;
-    });
-
-}
-
-
-/**
- * The service's entry point. It takes over the configuration
- * options and the logger- and metrics-reporting objects from
- * servisor and starts an HTTP server, attaching the application
- * object to it.
- */
-module.exports = function(options) {
-
-    var app;
-
-    // get the application object
-    return loadRoutes()
-    .then(function(app) {
         // get the options and make them available in the app
         app.logger = options.logger;    // the logging device
         app.metrics = options.metrics;  // the metrics
         app.conf = options.config;      // this app's config options
-        app.info = pkg_info;            // this app's package info
+        app.hostOptions = options;      // for additional access to service runner options
+        app.info = packageInfo;         // this app's package info
         // ensure some sane defaults
-        if(!app.conf.port) { app.conf.port = 8888; }
-        if(!app.conf.interface) { app.conf.interface = '0.0.0.0'; }
-        // return a promise which creates an HTTP server,
-        // attaches the app to it, and starts accepting
-        // incoming client requests
-        return new BBPromise(function(resolve) {
-            http.createServer(app).listen(
-                app.conf.port,
-                app.conf.interface,
-                resolve
-            );
-        }).then(function() {
-            app.logger.log('info', 'Worker ' + process.pid + ' listening on '
-                + app.conf.interface + ':' + app.conf.port);
-        });
+        if (!app.conf.port) {
+            app.conf.port = 8888;
+        }
+        if (!app.conf.interface) {
+            app.conf.interface = '0.0.0.0';
+        }
+        resolve(app);
     });
+}
+
+/**
+ * Async load all routes for the app
+ * @param app
+ * @returns {bluebird}
+ */
+function loadRoutes (app) {
+    // get the list of files in routes/
+    return fs
+        .readdirAsync(__dirname + '/routes')
+        .map(function (fname) {
+            // ... and then load each route
+            // but only if it's a js file
+            if (!/\.js$/.test(fname)) {
+                return;
+            }
+            // import the route file
+            var route = require(__dirname + '/routes/' + fname);
+            route = route(app);
+            // check that the route exports the object we need
+            if (route.constructor !== Object || !route.path || !route.router) {
+                throw new Error('routes/' + fname + ' does not export the correct object!');
+            }
+            // all good, use that route
+            app.use(route.path, route.router);
+        }).then(function () {
+            // route loading is now complete, return the app object
+            return app;
+        });
+}
+
+/**
+ * Async create a web server
+ * @param app
+ * @returns {bluebird}
+ */
+function createServer(app) {
+    // return a promise which creates an HTTP server,
+    // attaches the app to it, and starts accepting
+    // incoming client requests
+    return new BBPromise(function (resolve) {
+        http.createServer(app).listen(
+            app.conf.port,
+            app.conf.interface,
+            resolve
+        );
+    }).then(function () {
+            app.logger.log('info',
+                'Worker ' + process.pid + ' listening on ' +
+                app.conf.interface + ':' + app.conf.port);
+        });
+}
+
+/**
+ * The service's entry point. It takes over the configuration
+ * options and the logger- and metrics-reporting objects from
+ * supervisor and starts an HTTP server, attaching the application
+ * object to it.
+ */
+module.exports = function(options) {
+    return initApp(options)
+        .then(loadRoutes)
+        .then(createServer);
 };
