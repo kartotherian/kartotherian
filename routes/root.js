e3dc1ea848096ae7c09bcda480e60d0b813322e9
Marko Obrovac <marko.obrovac@gmail.com>
2015-06-02 12:06:53 +0200

    Expose the /_spec end-point for spec retrieval

--- routes/root.js
+++ routes/root.js
@@ -9,6 +9,11 @@ var sUtil = require('../lib/util');
  */
 var router = sUtil.router();
 
+/**
+ * The main application object reported when this module is require()d
+ */
+var app;
+
 
 /**
  * GET /robots.txt
@@ -24,7 +29,20 @@ router.get('/robots.txt', function(req, res) {
 });
 
 
+/**
+ * GET /_spec
+ * Retrieves the service's specification JSON
+ */
+router.get('/_spec', function(req, res) {
+
+    res.json(app.conf.spec);
+
+});
+
+
 module.exports = function(appObj) {
+
+    app = appObj;
 
     return {
         path: '/',
