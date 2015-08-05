88acc4e154024d0492d3de6fc249349c3ea59ced
Yuri Astrakhan <yurik@wikimedia.org>
2015-08-05 03:50:08 +0200

    Forked Kartotherian into a separate Tilerator service

--- scripts/download-water-polygons.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-
-echo 'Downloading Mercator Water Polygons from http://openstreetmapdata.com/data/water-polygons'
-wget http://data.openstreetmapdata.com/water-polygons-split-3857.zip
-
-echo 'Unzipping and deleting water-polygons-split-3857.zip'
-unzip water-polygons-split-3857 && rm water-polygons-split-3857.zip
