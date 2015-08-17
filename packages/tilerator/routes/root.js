a4a7e6ee11357fd2eaa255dcba813bdda1321fdc
Yuri Astrakhan <yurik@wikimedia.org>
2015-08-17 22:45:58 +0200

    Merge from service-template-node a6d44bb
    Conflicts:
    	app.js
    	config.dev.yaml
    	package.json

--- packages/tilerator/routes/root.js
+++ packages/tilerator/routes/root.js
@@ -9,6 +9,11 @@ var sUtil = require('../lib/util');
  */
 var router = sUtil.router();
 
+/**
+ * The main application object reported when this module is require()d
+ */
+var app;
+
 
 /**
  * GET /robots.txt
@@ -24,7 +29,25 @@ router.get('/robots.txt', function(req, res) {
 });
 
 
+/**
+ * GET /
+ * Main entry point. Currently it only responds if the spec query
+ * parameter is given, otherwise lets the next middleware handle it
+ */
+router.get('/', function(req, res, next) {
+
+    if(!(req.query || {}).hasOwnProperty('spec')) {
+        next();
+    } else {
+        res.json(app.conf.spec);
+    }
+
+});
+
+
 module.exports = function(appObj) {
+
+    app = appObj;
 
     return {
         path: '/',
