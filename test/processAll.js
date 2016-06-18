54adc0bfae2c7509ae3508378570c14539f95621
Yuri Astrakhan <yurik@wikimedia.org>
2016-06-18 05:53:52 +0300

    Fixed missing zoom for file job processing

--- test/processAll.js
+++ test/processAll.js
@@ -2,13 +2,13 @@
 
 var Promise = require('bluebird');
 var assert = require('assert');
-var utils = require('./testUtils');
 var processAllLib = require('../lib/processAll');
 var fileParserLib = require('../lib/fileParser');
 var pathLib = require('path');
 var util = require('util');
 var fs = require('fs');
 var core = require('kartotherian-core');
+var Job = require('../lib/Job');
 
 var stateFile = pathLib.resolve(__dirname, 'data/stat.txt');
 var expDirPath = pathLib.resolve(__dirname, 'data');
@@ -74,7 +74,8 @@ describe('processAll', function() {
             fs.writeFileSync(stateFile, stateData, 'utf8');
         }
         return processAllLib(expDirPath, stateFile, mask, opts, function (job) {
-            // The job object is mutable, need to deep clone it
+            // test job's params
+            new Job(job);
             addedJobs.push(job);
         }).then(function () {
             debug('cleanup %j', tempFiles);
@@ -98,16 +99,22 @@ describe('processAll', function() {
 
     it('single file', function () {
         return test(U, '01-15-ok', '01-15-ok.dat', ['01-15-ok.dat'],
-            {"storageId": "sid", "generatorId": "gid", "tiles": [42502815, [42502822, 42502830]]}
+            {
+                storageId: "sid",
+                generatorId: "gid",
+                zoom: 15,
+                tiles: [42502815, [42502822, 42502830]]
+            }
         );
     });
 
     it('all same zoom', function () {
         return test(U, '-15-ok', '02-15-ok.dat', ['01-15-ok.dat', '02-15-ok.dat'],
             {
-                "storageId": "sid",
-                "generatorId": "gid",
-                "tiles": [42502815, [42502822, 42502830], 928255040, [928255124, 928255126], [928255127, 928255132], [928255152, 928255154]]
+                storageId: "sid",
+                generatorId: "gid",
+                zoom: 15,
+                tiles: [42502815, [42502822, 42502830], 928255040, [928255124, 928255126], [928255127, 928255132], [928255152, 928255154]]
             });
     });
 
@@ -115,25 +122,32 @@ describe('processAll', function() {
         return Promise.resolve().then(function () {
             return test(U, '-ok', '02-15-ok.dat', ['01-15-ok.dat', '02-15-ok.dat', '03-16-ok.dat'],
                 {
-                    "storageId": "sid",
-                    "generatorId": "gid",
-                    "tiles": [42502815, [42502822, 42502830], 928255040, [928255124, 928255126], [928255127, 928255132], [928255152, 928255154]]
+                    storageId: "sid",
+                    generatorId: "gid",
+                    zoom: 15,
+                    tiles: [42502815, [42502822, 42502830], 928255040, [928255124, 928255126], [928255127, 928255132], [928255152, 928255154]]
                 });
         }).then(function () {
             tempFiles = {};
             addedJobs = [];
             return test(U, '-ok', '03-16-ok.dat', ['03-16-ok.dat'],
                 {
-                    "storageId": "sid",
-                    "generatorId": "gid",
-                    "tiles": [170011291, 170011299, [170011301, 170011307]]
+                    storageId: "sid",
+                    generatorId: "gid",
+                    zoom: 16,
+                    tiles: [170011291, 170011299, [170011301, 170011307]]
                 });
         });
     });
 
     it('bad file', function () {
         return test(U, '-bad', '01-15-ok.dat', ['01-15-ok.dat'],
-            {"storageId": "sid", "generatorId": "gid", "tiles": [42502815, [42502822, 42502830]]}
+            {
+                storageId: "sid",
+                generatorId: "gid",
+                zoom: 15,
+                tiles: [42502815, [42502822, 42502830]]
+            }
         ).then(function () {
             throw new Err('must have failed')
         }, function () {
