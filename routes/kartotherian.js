5f2308736e4d329a2d01f9a49060081d42aeb81f
Yuri Astrakhan <yurik@wikimedia.org>
2016-08-18 04:23:04 +0300

    Use bootstrap func to init
    Change-Id: I3988ed4817b36ea389ba4de0b5c3b0e55f10d107

--- routes/kartotherian.js
+++ routes/kartotherian.js
@@ -5,18 +5,12 @@ var Promise = require('bluebird');
 var core = require('kartotherian-core');
 var info = require('../package.json');
 
-module.exports = function(app) {
+module.exports = startup;
 
-    return Promise.try(function () {
-        core.init(app, info.kartotherian, pathLib.resolve(__dirname, '..'),
-            function (module) {
-                return require(module);
-            },
-            function (module) {
-                return require.resolve(module);
-            }
-        );
-        var sources = new core.Sources(app);
+function startup(app) {
+
+    return startup.bootstrap(app).then(function() {
+        var sources = new core.Sources();
         return sources.init(app.conf.variables, app.conf.sources);
     }).then(function (sources) {
         core.setSources(sources);
@@ -26,4 +20,17 @@ module.exports = function(app) {
             requestHandlers: core.loadNpmModules('requestHandlers')
         });
     }).return(); // avoid app.js's default route initialization
+}
+
+startup.bootstrap = function bootstrap(app) {
+    return Promise.try(function () {
+        core.init(app, info.kartotherian, pathLib.resolve(__dirname, '..'),
+            function (module) {
+                return require(module);
+            },
+            function (module) {
+                return require.resolve(module);
+            }
+        );
+    });
 };
