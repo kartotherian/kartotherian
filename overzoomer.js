464c8ec77a9fd5cc9b54ad445a29d4d9041ee409
Yuri Astrakhan <yurik@wikimedia.org>
2016-10-18 23:02:41 -0400

    Cleanup, bump to ES6

--- overzoomer.js
+++ overzoomer.js
@@ -6,54 +6,47 @@ OverZoomer is a storage wrapper. Given a tile source, it will retrieve requested
 or if missing, will zoom out until it finds a tile, and extract needed portion of it.
  */
 
-var Promise = require('bluebird');
-var zlib = require('zlib');
-var core, Err;
+let Promise = require('bluebird'),
+    zlib = require('zlib'),
+    core, Err;
 
 
 function OverZoomer(uri, callback) {
-    var self = this;
-    return Promise.try(function () {
-        var params = core.normalizeUri(uri).query;
+    let self = this;
+    return Promise.try(() => {
+        let params = core.normalizeUri(uri).query;
         if (!params.source) {
             throw new Err("Uri must include 'source' query parameter: %j", uri);
         }
         self.minzoom = typeof params.minzoom === 'undefined' ? 0 : parseInt(params.minzoom);
         self.maxzoom = typeof params.maxzoom === 'undefined' ? 22 : parseInt(params.maxzoom);
         return core.loadSource(params.source);
-    }).then(function (handler) {
+    }).then(handler => {
         self.source = handler;
         return self;
     }).nodeify(callback);
 }
 
-OverZoomer.prototype.getTile = function(z, x, y, callback) {
-    var self = this,
+OverZoomer.prototype.getTile = function getTile(z, x, y, callback) {
+    let self = this,
         bz = z,
         bx = x,
         by = y;
 
-    return getSubTile().spread(function (pbfz, headers) {
-        if (bz === z || !pbfz || pbfz.length === 0) {
-            // this is exactly what we were asked for initially
-            return [pbfz, headers];
+    return getSubTile().spread((pbfz, headers) => {
+        if (bz !== z) {
+            headers.OverzoomFrom = bz;
         }
-        // Extract portion of the higher zoom tile as a new tile
-        headers.OverzoomFrom = bz;
-        return core.uncompressAsync(pbfz).then(function (pbf) {
-            return core.extractSubTileAsync(pbf, z, x, y, bz, bx, by);
-        }).then(function (pbf) {
-            return core.compressPbfAsync2(pbf, headers);
-        });
+        return [pbfz, headers];
     }).nodeify(callback, {spread: true});
 
     function getSubTile() {
-        return Promise.try(function () {
+        return Promise.try(() => {
             if (bz < self.minzoom || bz > self.maxzoom) {
                 core.throwNoTile();
             }
             return self.source.getTileAsync(bz, bx, by);
-        }).catch(function (err) {
+        }).catch(err => {
             if (bz > self.minzoom && core.isNoTileError(err)) {
                 // Tile is missing, zoom out and repeat
                 bz = bz - 1;
@@ -67,12 +60,12 @@ OverZoomer.prototype.getTile = function(z, x, y, callback) {
     }
 };
 
-OverZoomer.prototype.getInfo = function(callback) {
+OverZoomer.prototype.getInfo = function getInfo(callback) {
     return this.source.getInfo(callback);
 };
 
 
-OverZoomer.initKartotherian = function(cor) {
+OverZoomer.initKartotherian = function initKartotherian(cor) {
     core = cor;
     Err = core.Err;
     core.tilelive.protocols['overzoom:'] = OverZoomer;
