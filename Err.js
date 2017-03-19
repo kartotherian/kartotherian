813c52a67ca45fb98d7a3696e149a3d1ce66f577
Yuri Astrakhan <yurik@wikimedia.org>
2017-03-19 16:14:25 -0400

    added jcsc and travis

--- Err.js
+++ Err.js
@@ -11,7 +11,9 @@ let util = require('util');
 function Err(message) {
     Error.captureStackTrace(this, this.constructor);
     this.name = this.constructor.name;
-    this.message = arguments.length < 2 ? (message || 'unknown') : util.format.apply(null, arguments);
+    this.message = arguments.length < 2
+        ? (message || 'unknown')
+        : util.format.apply(null, arguments);
 }
 
 util.inherits(Err, Error);
