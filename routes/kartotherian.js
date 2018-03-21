7341a40545c0e0a31e116bc19cd2a1fd810f1157
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2018-03-21 11:26:59 -0400

    [WIP] Use npmi module loader
    Change-Id: I9921240c2375c0d878d542d136ec826f48ada8f8

--- routes/kartotherian.js
+++ routes/kartotherian.js
@@ -1,36 +1,63 @@
 'use strict';
 
-let pathLib = require('path'),
-    Promise = require('bluebird'),
-    core = require('@kartotherian/core'),
-    info = require('../package.json');
+const pathLib = require('path');
+const Promise = require('bluebird');
+const core = require('@kartotherian/core');
+const npmi = Promise.promisify(require('npmi'));
+const info = require('../package.json');
 
 module.exports = startup;
 
 function startup(app) {
 
-    return startup.bootstrap(app).then(function() {
+    return startup.bootstrap(app).then(() => {
         let sources = new core.Sources();
-        return sources.init(app.conf.variables, app.conf.sources);
-    }).then(function (sources) {
+        return sources.init(app.conf);
+    }).then(sources => {
         core.setSources(sources);
         return require('@kartotherian/server').init({
             core: core,
             app: app,
-            requestHandlers: core.loadNpmModules('requestHandlers')
+            // requestHandlers: core.loadNpmModules('requestHandlers')
         });
     }).return(); // avoid app.js's default route initialization
 }
 
 startup.bootstrap = function bootstrap(app) {
-    return Promise.try(function () {
-        core.init(app, info.kartotherian, pathLib.resolve(__dirname, '..'),
-            function (module) {
-                return require(module);
-            },
-            function (module) {
-                return require.resolve(module);
-            }
-        );
-    });
+    return Promise.try(
+      () => core.init(app, pathLib.resolve(__dirname, '..'), loadModule, module => require.resolve(module))
+    );
 };
+
+function loadModule(module) {
+  return Promise.try(
+    () => require(module)
+  ).catch(err => {
+    console.error(`Unable to load module "${module}", attempting to install from NPM`);
+    console.error(err);
+    return installNpmModule(module).then(() => require(module));
+  });
+}
+
+function installNpmModule(module) {
+
+  const options = {
+    name: module,
+    // npmLoad: {
+    //   // as defined in https://docs.npmjs.com/misc/config
+    //   loglevel: 'silent'
+    // }
+  };
+
+  return npmi(options).then(result => {
+    console.error(options.name + '@' + options.version + ' installed successfully in ' + path.resolve(options.path));
+    console.error(`** NOTE: For production, it is recommended to pre-install required external NPM packages`);
+    return result;
+  }, err => {
+    if (err.code === npmi.LOAD_ERR)
+      console.error(`npm load error`);
+    else if (err.code === npmi.INSTALL_ERR)
+      console.error(`npm install error`);
+    console.error(err.message);
+  });
+}
